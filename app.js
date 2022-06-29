50c1a1f07f27059866332745a882d74012872943
Kartik Nahta <kartiknahta@gmail.com>
2022-06-30 00:09:49 +0530

    Secrets page and option set up

--- app.js
+++ app.js
@@ -117,11 +117,39 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/secrets", function(req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res) {
     if(req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, foundUser) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 
 app.get("/logout", function(req, res){
@@ -132,11 +160,6 @@ app.get("/logout", function(req, res){
     });
     res.redirect("/");
 });
-
-// app.get("/logout", function(req, res) {
-//     req.logout();
-//     res.redirect("/");
-// });
 
 app.post("/register", function(req, res) {
     User.register({username: req.body.username}, req.body.password, function(err, user) {
